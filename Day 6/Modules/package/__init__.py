b8b4aa49d1ecef8497d1466d4e002456804d7ca3
Subodh Raut <subodhrautssr7@gmail.com>
2026-08-04 01:49:39 +0530

    Day 6: Completed Python Modules, Packages, Subpackages and Imports

---
+++ Day 6/Modules/package/__init__.py
@@ -0,0 +1,5 @@
+''' __init__.py file for the package module. 
+This file is executed when the package is imported, and it can be used to initialize 
+the package or define package-level variables and functions. In this case, the file is empty,
+indicating that there are no specific initializations or definitions needed for this package. '''
+
